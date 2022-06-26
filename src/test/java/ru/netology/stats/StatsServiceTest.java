cf62c8761618e328f63b6252fae342566138acd9
Shakhana <shamilova.08@gmail.com>
2022-06-26 23:52:20 +0300

    Initial commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,74 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldCalculateSum() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        int actual = service.Sum(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateAvg() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+
+        int actual = service.Sum(sales) / sales.length;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMaxSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+
+        int actual = service.maxSales(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMinSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        int actual = service.minSales(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateNumberOfMonthMinSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.amountOfMonthsMinSales(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateNumberOfMonthMaxSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.amountOfMonthsMaxSales(sales);
+
+        assertEquals(expected, actual);
+    }
+}
